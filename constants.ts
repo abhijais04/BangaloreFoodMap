b41eca05d6543779b35e8a4818bd2502b8773e39
Abhishek Jaiswal <abhijais04@gmail.com>
2026-03-26 00:27:29 +0530

    adding more places

--- constants.ts
+++ constants.ts
@@ -1,7 +1,7 @@
 
 import { FoodPlace } from './types';
 
-export const BANGALORE_CENTER = { lat: 12.9716, lng: 77.5946 };
+export const BANGALORE_CENTER = { lat: 12.997381887241968, lng: 77.66144310199624 };
 
 export const INITIAL_PLACES: FoodPlace[] = [
   {
@@ -92,6 +92,91 @@ export const INITIAL_PLACES: FoodPlace[] = [
     { id: 'd1', name: 'Salad Pizza with Burrata on top' },
     { id: 'd2', name: 'Crab Spaghetti' },
     { id: 'd3', name: 'Four cheese Pizza' },
+  ],
+  },
+  {
+    id: '6',
+    name: 'Seetaphal Fresh Juices',
+    description: 'Tasty Fruit based desserts and shakes',
+    category: 'Fruit Desserts',
+    rating: 4.2,
+    lat: 12.979093648467375,
+    lng: 77.69536428465815,
+    imageUrl: 'https://picsum.photos/seed/mtr/600/400',
+    address: 'Doddanekundi, Bangalore',
+    reviews: [
+    ],
+  dishes: [
+    { id: 'd1', name: 'Seetaphal Cream' },
+    { id: 'd2', name: 'Dryfruit Milkshake' },
+  ],
+  },
+  {
+    id: '7',
+    name: 'Litti House',
+    description: 'Tasty Chicken curry and other dishes with flavours of Bihar',
+    category: 'North Indian',
+    rating: 4.4,
+    lat: 12.961995126842858,
+    lng: 77.71696954787366,
+    imageUrl: 'https://picsum.photos/seed/mtr/600/400',
+    address: 'Brookefield, Bangalore',
+    reviews: [
+    ],
+  dishes: [
+    { id: 'd1', name: 'Chicken Curry' },
+    { id: 'd2', name: 'Hariyali Tikka' },
+  ],
+  },
+  {
+    id: '8',
+    name: 'Pandit Ji - Ras Bana Rahe',
+    description: 'Simple North Indian Confort Food',
+    category: 'North Indian',
+    rating: 4.4,
+    lat: 12.972304807920564,
+    lng:  77.64218058465816,
+    imageUrl: 'https://picsum.photos/seed/mtr/600/400',
+    address: 'Indiranagar, Bangalore',
+    reviews: [
+    ],
+  dishes: [
+    { id: 'd1', name: 'Puri Sabzi' },
+    { id: 'd2', name: 'Lassi' },
+  ],
+  },
+  {
+    id: '9',
+    name: 'Amritsari Kulcha Land',
+    description: 'Best Amritsari Kulchas in Bangalore',
+    category: 'North Indian',
+    rating: 4.7,
+    lat: 12.900123251472822,
+    lng:  77.5858429981518,
+    imageUrl: 'https://picsum.photos/seed/mtr/600/400',
+    address: 'Jayanagar, Bangalore',
+    reviews: [
+    ],
+  dishes: [
+    { id: 'd1', name: 'Amritsari Aaloo Kucha' },
+    { id: 'd2', name: 'Amritsari Mixed Kulcha' },
+  ],
+  },
+  {
+    id: '10',
+    name: 'Belle-Ville Pancake Cafe',
+    description: 'Delicious fluffy Pancakes with a variety of toppings and fillings',
+    category: 'Cafe, Japanese',
+    rating: 4.8,
+    lat: 12.998000897978024, 
+    lng:  77.69644341534182,
+    imageUrl: 'https://picsum.photos/seed/mtr/600/400',
+    address: 'Whitefield, Bangalore',
+    reviews: [
+    ],
+  dishes: [
+    { id: 'd1', name: 'Mixed berry pancake' },
+    { id: 'd2', name: 'Red hot chicken sandwich' },
   ],
   },
 ];
